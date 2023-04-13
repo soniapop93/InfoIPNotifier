f7e400775392b9022691459b723b30d802025e05
soniapop93 <popsonia85@gmail.com>
2023-04-13 16:41:41 +0300

    implemented to send a notification on windows

---
+++ InfoIPNotifier/IP/GetInfoFromIP.cs
@@ -0,0 +1,23 @@
+﻿using InfoIPNotifier.Utilities;
+using System;
+using System.Text.Json;
+
+namespace InfoIPNotifier.IP
+{
+    public class GetInfoFromIP
+    {
+        public static string getInfoFromIPRequest(string endpoint)
+        {
+            string response = RequestManager.getRequest(endpoint);
+            return response;
+        }
+
+        public static InformationFromIP parseInfoFromIPRequest(string response)
+        {
+            InformationFromIP infoFromIP = JsonSerializer.Deserialize<InformationFromIP>(response);
+
+            return infoFromIP;
+        }
+
+    }
+}
---
+++ InfoIPNotifier/Utilities/CreateFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoIPNotifier.Utilities
+{
+    public class CreateFile
+    {
+        public static void generateFile(string text, string path)
+        {
+            string fileName = String.Format("YourIP_Information_{0}", DateTime.Now.ToString("dd_MM_yyyy"));
+            Console.WriteLine("Will generate new text file with name: {0}", fileName);
+
+            
+        }
+    }
+}
