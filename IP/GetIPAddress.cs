980fb4b27e06299458a234edbcc6fc88ce6c3afd
soniapop93 <popsonia85@gmail.com>
2023-04-13 12:18:20 +0300

    first commit. Implemented the get ip part. Todo next: send windows notification

---
+++ IP/GetIPAddress.cs
@@ -0,0 +1,15 @@
+﻿using InfoIPNotifier.Utilities;
+
+namespace InfoIPNotifier.IP
+{
+    public class GetIPAddress
+    {
+        public static IPAddress getIPAddress(string endpoint)
+        {
+            string response = RequestManager.getRequest(endpoint);
+            IPAddress ipAddress = new IPAddress(response);
+
+            return ipAddress;
+        }
+    }
+}
---
+++ IP/InformationFromIP.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InfoIPNotifier.IP
+{
+    public class InformationFromIP
+    {
+        public string status { get; set; }
+        public string continent { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string timezone { get; set; }
+        public string currency { get; set; }
+        public string isp { get; set; }
+        public string org { get; set; }
+        [JsonPropertyName("as")]
+        public string asn { get; set; }
+        public string reverse { get; set; }
+        public bool mobile { get; set; }
+        public bool proxy { get; set; }
+        public string ip { get; set; }
+        public bool cached { get; set; }
+        public long cacheTimestamp { get; set; }
+    }
+}
